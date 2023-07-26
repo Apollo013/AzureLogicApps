6f1fc8c14dc859e0066a78d57be4dadc891edc8f
@Apollo013 <paul.j.millar@gmail.com>
2023-07-26 16:10:04 +0100

    Add project files.

---
+++ AzureLogicApps/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AzureLogicApps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace AzureLogicApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest request, IFormFile file)
+        {
+            request.Id = Guid.NewGuid().ToString();
+            request.Status = "Submitted";
+
+            var jsonContent = JsonConvert.SerializeObject(request);
+            using (var content = new StringContent(jsonContent, Encoding.UTF8, "application/json"))
+            {
+                HttpResponseMessage httpResponse = await client.PostAsync("", content);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureLogicApps/Data/DbDataContext.cs
@@ -0,0 +1,27 @@
+﻿using AzureLogicApps.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AzureLogicApps.Data
+{
+    public class DbDataContext : DbContext
+    {
+        public DbDataContext(DbContextOptions<DbDataContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<SalesRequest> SalesRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SalesRequest>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+            });
+
+        }
+    }
+}
